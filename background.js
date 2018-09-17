70bf976619fff059d37c010c22536bda16f4a39e
teppeis <teppeis@gmail.com>
2018-09-17 11:59:31 +0900

    refactor: fix for eslint and prettier

--- background.js
+++ background.js
@@ -1,27 +1,25 @@
+'use strict';
+
 console.log('Tab counter: background.js');
 
-var green = [129,214,116,255];
-var yellow = [235,241,130,255];
-var red = [218,98,114,255];
+const green = [129, 214, 116, 255];
+const yellow = [235, 241, 130, 255];
+const red = [218, 98, 114, 255];
 
-var colors = [
-  {count: 0, color: green},
-  {count: 20, color: yellow},
-  {count: 30, color: red}
-];
+const colors = [{count: 0, color: green}, {count: 20, color: yellow}, {count: 30, color: red}];
 
 function updateCount() {
-  chrome.tabs.query({}, function(tabs) {
-    var count = String(tabs.length);
+  chrome.tabs.query({}, tabs => {
+    const count = String(tabs.length);
     chrome.browserAction.setTitle({title: count});
     chrome.browserAction.setBadgeText({text: count});
-    var color = getColor(tabs.length);
+    const color = getColor(tabs.length);
     chrome.browserAction.setBadgeBackgroundColor({color: color});
   });
 }
 
 function getColor(count) {
-  for (var i = colors.length - 1; i < colors.length && i >= 0; i--) {
+  for (let i = colors.length - 1; i < colors.length && i >= 0; i--) {
     if (colors[i].count <= count) {
       return colors[i].color;
     }
@@ -32,5 +30,5 @@ function getColor(count) {
 chrome.runtime.onInstalled.addListener(updateCount);
 chrome.tabs.onCreated.addListener(updateCount);
 chrome.tabs.onRemoved.addListener(updateCount);
-//chrome.tabs.onAttached.addListener(updateCount);
-//chrome.tabs.onDetached.addListener(updateCount);
+// chrome.tabs.onAttached.addListener(updateCount);
+// chrome.tabs.onDetached.addListener(updateCount);
